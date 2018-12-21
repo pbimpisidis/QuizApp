0f4002f4c0db2e083c2f2b40e748220b300c81b1
paschalisbimpisidis <pbimpisidis@gmail.com>
2018-12-21 20:48:48 +0100

    removed duplicates

--- src/main/java/com/example/paschalisbimpisidis/quizapp/MainActivity.java
+++ src/main/java/com/example/paschalisbimpisidis/quizapp/MainActivity.java
@@ -23,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
         boolean four = isAnswer2Correct();
         boolean five = isAnswer2Correct();
         Log.v("MainActivity", "1st answer is: " + one + ", 2nd answer is: " + two + ", 3rd answer is: " + three + ", 4th answer is: " + four + ", 5th answer is: " + five);
-        Log.v("MainActivity", "1st answer is: " + one + ", 2nd answer is: " + two + ", 3rd answer is: " + three + ", 4th answer is: " + four + ", 5th answer is: " + five);
-        Log.v("MainActivity", "1st answer is: " + one + ", 2nd answer is: " + two + ", 3rd answer is: " + three + ", 4th answer is: " + four + ", 5th answer is: " + five);
     }
 
     private boolean isAnswer1Correct() {
