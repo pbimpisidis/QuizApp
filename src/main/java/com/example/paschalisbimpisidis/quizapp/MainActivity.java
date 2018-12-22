094216967b307f1e5e33d44c36025e2c0f385458
paschalisbimpisidis <pbimpisidis@gmail.com>
2018-12-22 11:32:30 +0100

    initial commit

--- src/main/java/com/example/paschalisbimpisidis/quizapp/MainActivity.java
+++ src/main/java/com/example/paschalisbimpisidis/quizapp/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.paschalisbimpisidis.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,28 +18,87 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getResults(View view) {
-        boolean one = isAnswer1Correct();
-        boolean two = isAnswer2Correct();
-        boolean three = isAnswer3Correct();
-        boolean four = isAnswer2Correct();
-        boolean five = isAnswer2Correct();
-        Log.v("MainActivity", "1st answer is: " + one + ", 2nd answer is: " + two + ", 3rd answer is: " + three + ", 4th answer is: " + four + ", 5th answer is: " + five);
+        int points = 0;
+        boolean isAnswer1Correct = answer1();
+        boolean isAnswer2Correct = answer2();
+        boolean isAnswer3Correct = answer3();
+        boolean isAnswer4Correct = answer4();
+        boolean isAnswer5Correct = answer5();
+
+        if (isAnswer1Correct)
+            points += 1;
+        if (isAnswer2Correct)
+            points += 1;
+        if (isAnswer3Correct)
+            points += 1;
+        if (isAnswer4Correct)
+            points += 1;
+        if (isAnswer5Correct)
+            points += 1;
+
+        displayResult(points);
     }
 
-    private boolean isAnswer1Correct() {
+    private void displayResult(int points) {
+
+        String scoreMessage = "Result: " + points + " out of 5!\n";
+        String successMessage = displaySuccessMessage();
+        String failMessage = displayFailMessage();
+
+        if (points == 5)
+            Toast.makeText(this, scoreMessage + successMessage, Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, scoreMessage + failMessage, Toast.LENGTH_LONG).show();
+    }
+
+    private String displaySuccessMessage() {
+        return getString(R.string.successMessage);
+
+    }
+
+    private String displayFailMessage() {
+        return getString(R.string.failMessage);
+
+    }
+
+    private boolean answer1() {
         EditText input = findViewById(R.id.answer1);
-        String answer1 = input.getText().toString();cddsfgfdsg
+        String answer1 = input.getText().toString();
 
         return answer1.equals("5");
     }
 
-    private boolean isAnswer2Correct() {
-        RadioGroup answer2 = findViewById(R.id.question2);
-        int selectedId = answer2.getCheckedRadioButtonId();
-        RadioButton option2 = findViewById(R.id.answer2_2);
+    private boolean answer2() {
+        RadioGroup answer2 = findViewById(R.id.answer2);
+        int selectedAnswerId = answer2.getCheckedRadioButtonId();
+        RadioButton correctAnswer = findViewById(R.id.answer2_2);
 
-        return selectedId == option2.getId();
+        return selectedAnswerId == correctAnswer.getId();
     }
 
+    private boolean answer3() {
+        RadioGroup answer3 = findViewById(R.id.answer3);
+        int selectedAnswerId = answer3.getCheckedRadioButtonId();
+        RadioButton correctAnswer = findViewById(R.id.answer3_3);
+
+        return selectedAnswerId == correctAnswer.getId();
+    }
+
+    private boolean answer4() {
+        CheckBox answer4_1 = findViewById(R.id.answer4_1);
+        CheckBox answer4_2 = findViewById(R.id.answer4_2);
+        CheckBox answer4_3 = findViewById(R.id.answer4_3);
+        CheckBox answer4_4 = findViewById(R.id.answer4_4);
+
+        return (!answer4_1.isChecked() && answer4_2.isChecked() && answer4_3.isChecked() && !answer4_4.isChecked());
+    }
+
+    private boolean answer5() {
+        RadioGroup answer5 = findViewById(R.id.answer5);
+        int selectedAnswerId = answer5.getCheckedRadioButtonId();
+        RadioButton correctAnswer = findViewById(R.id.answer5_1);
+
+        return selectedAnswerId == correctAnswer.getId();
+    }
 
 }
